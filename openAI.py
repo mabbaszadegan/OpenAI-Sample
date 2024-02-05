7f5c5ccf4950bf66c9b6015545c57f5abe08e8e8
mabbaszadegan <mhmabbaszadegan@gmail.com>
2024-02-05 10:30:12 +0330

    modify sql structure

--- openAI.py
+++ openAI.py
@@ -18,42 +18,9 @@ class openAIHelper:
       messages=[
         {
           "role": "system",
-          "content": '''Given the following SQL tables, 
+          "content": f'''Given the following SQL tables, 
                         your job is to write queries given a user’s request.
-                        
-                        CREATE TABLE Orders (
-                          OrderID int,
-                          CustomerID int,
-                          OrderDate datetime,
-                          OrderTime varchar(8),
-                          PRIMARY KEY (OrderID)
-                        );
-                        
-                        CREATE TABLE OrderDetails (
-                          OrderDetailID int,
-                          OrderID int,
-                          ProductID int,
-                          Quantity int,
-                          PRIMARY KEY (OrderDetailID)
-                        );
-
-                        CREATE TABLE Products (
-                          ProductID int,
-                          ProductName varchar(50),
-                          Category varchar(50),
-                          UnitPrice decimal(10, 2),
-                          Stock int,
-                          PRIMARY KEY (ProductID)
-                        );
-
-                        CREATE TABLE Customers (
-                          CustomerID int,
-                          FirstName varchar(50),
-                          LastName varchar(50),
-                          Email varchar(100),
-                          Phone varchar(20),
-                          PRIMARY KEY (CustomerID)
-                        );
+                        {os.getenv("sql_schema")}
                         '''
         },
         {
@@ -77,7 +44,7 @@ class openAIHelper:
       if len(scripts) == 0:
         script = gpt_result
       else:
-        script = scripts[0].upper().replace('#N#',' ').replace('#N',' ')
+        script = scripts[0].upper().replace('#N#','\n ').replace('#N','\n ')
       
       if("DELETE" in script or "UPDATE" in script or "INSERT" in script):
         return Result.fail(ErrorResult("متن شما شامل دستور غیرمجاز است"))
